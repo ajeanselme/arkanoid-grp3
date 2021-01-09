d0e6fbf46e9701a058a266a1e2a9aa2f0d537139
illianes <pierremariotte98@mail.com>
2021-01-09 22:41:20 +0100

    Movement 2 players

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public bool isPlayer1;
+    public float speed;
+    public Rigidbody2D rb;
+
+    private float movement;
+
+
+    void Update()
+    {
+        if (isPlayer1)
+        {
+            movement = Input.GetAxisRaw("Horizontal");
+        }
+        else
+        {
+            movement = Input.GetAxisRaw("Horizontal2");
+        }
+
+        rb.velocity = new Vector2(rb.velocity.y, movement * speed);
+    }
+}
